1b5466abe5094ad322490bd75bcc85d1cad37142
Hiroaki-Kubo <hiroaki.kubo@pixiedusttech.com>
2022-09-04 21:04:41 +0900

    Add constructor and base

--- includes/stl_iterator.hpp
+++ includes/stl_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 16:46:18 by hkubo             #+#    #+#             */
-/*   Updated: 2022/09/04 18:00:11 by hkubo            ###   ########.fr       */
+/*   Updated: 2022/09/04 21:01:38 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,12 @@ namespace ft
             typedef typename ft::iterator_traits<Iterator>::pointer pointer;
             typedef typename ft::iterator_traits<Iterator>::reference reference;
 
-            reverse_iterator()
-            {
-                
-            }
-            explicit reverse_iterator (iterator_type it)
-            {
-                (void)it;
-            }
+            reverse_iterator() : current() {};
+            explicit reverse_iterator(iterator_type it) : current(it) {};
             template <class Iter>
-            reverse_iterator (const reverse_iterator<Iter> &rev_it)
-            {
-                (void)rev_it;
-            }
+            reverse_iterator(const reverse_iterator<Iter> &rev_it): current(rev_it.base()) {};
+
+            iterator_type base() const {return current;}
 
         protected:
             Iterator current;
--- tester/test_reverse_iterator.cpp
+++ tester/test_reverse_iterator.cpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 16:47:44 by hkubo             #+#    #+#             */
-/*   Updated: 2022/09/04 17:48:14 by hkubo            ###   ########.fr       */
+/*   Updated: 2022/09/04 21:03:22 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,33 @@ void test_reverse_iterator()
     std::cout << std::endl;
     std::cout << "~~~ TEST reverse_iterator ~~~" << std::endl;
 
-    std::cout << "constructor test" << std::endl;
-    std::vector<int> myvector;
-    for (int i = 0; i < 10; i++)
-        myvector.push_back(i);
-    typedef std::vector<int>::iterator iter_type;
-    iter_type from(myvector.begin());
-    iter_type until(myvector.end());
-    ft::reverse_iterator<iter_type> rev_until(from);
-    // std::reverse_iterator<iter_type> rev_from(until);
-    // std::cout << "myvector:";
-    // while (rev_from != rev_until)
-    //     std::cout << ' ' << *rev_from++;
-    // std::cout << std::endl;
+    {
+        std::cout << "constructor test" << std::endl;
+        std::vector<int> myvector;
+        for (int i = 0; i < 10; i++)
+            myvector.push_back(i);
+        typedef std::vector<int>::iterator iter_type;
+        iter_type from(myvector.begin());
+        iter_type until(myvector.end());
+        ft::reverse_iterator<iter_type> rev_until(from);
+        std::reverse_iterator<iter_type> rev_from(until);
+        // std::cout << "myvector:";
+        // while (rev_from != rev_until)
+        //     std::cout << ' ' << *rev_from++;
+        std::cout << std::endl;
+    }
+
+    {
+        std::cout << "base test" << std::endl;
+        std::vector<int> myvector;
+        for (int i = 0; i < 10; i++)
+            myvector.push_back(i);
+        typedef std::vector<int>::iterator iter_type;
+        ft::reverse_iterator<iter_type> rev_end(myvector.begin());
+        ft::reverse_iterator<iter_type> rev_begin(myvector.end());
+        std::cout << "myvector:";
+        for (iter_type it = rev_end.base(); it != rev_begin.base(); ++it)
+            std::cout << ' ' << *it;
+        std::cout << std::endl;
+    }
 }
